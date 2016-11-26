9ccb9d4b1f029047f8d2e9da318ec254c1172a55
vaidhya mookiah <vaidhyaM@Shakthi.local>
2016-11-26 19:25:04 +1100

    Finished with User Interface. Added two different cell for with image and without image. Webview controller added

---
+++ ArticleReader/CellWithImage.swift
@@ -0,0 +1,24 @@
+//
+//  CellWithImage.swift
+//  ArticleReader
+//
+//  Created by vaidhya mookiah on 26/11/16.
+//  Copyright © 2016 Shakthi. All rights reserved.
+//
+
+import UIKit
+
+class CellWithImage: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ArticleReader/CellWithoutImage.swift
@@ -0,0 +1,24 @@
+//
+//  CellWithoutImage.swift
+//  ArticleReader
+//
+//  Created by vaidhya mookiah on 26/11/16.
+//  Copyright © 2016 Shakthi. All rights reserved.
+//
+
+import UIKit
+
+class CellWithoutImage: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
